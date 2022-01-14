6d18cb432336ab94b551e8650ee1178bc49e74a0
Karl Telle <karl.wilhelm.telle@gmail.com>
2022-01-14 08:44:43 +0100

    add ui for task 1

--- Aufgabe2/1/aufgabe2-1/ui.R
+++ Aufgabe2/1/aufgabe2-1/ui.R
@@ -1,3 +1,9 @@
 shinyUI(fluidPage(
-  navbarPage("Aufgabe 2 - Teil 1")
+  navbarPage("Aufgabe 2 - Teil 1"),
+  mainPanel(plotOutput("plotgraph", "500px", "500px"),
+            textOutput("cvMedian"),
+            verbatimTextOutput("confusionMatrixCV"),
+            plotOutput("plotgraphBoot", "500px", "500px"),
+            textOutput("bootMedian"),
+            verbatimTextOutput("confusionMatrixBoot"))
 ))
